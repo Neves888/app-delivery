f96c8a6c1c58a55ac0fbc32c70a87277117d154d
Hugo Flávio <hugoflavio@live.com>
2023-01-23 16:46:07 -0300

    Coorigido rotas da aplicação de index para App

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,21 +1,12 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 import App from './App';
-import Login from './pages/Login';
-import Register from './pages/Register';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route exact path="/" element={ <App /> } />
-        <Route exact path="/login" element={ <Login /> } />
-        <Route exact path="/register" element={ <Register /> } />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
   document.getElementById('root'),
 );
--- front-end/src/pages/Register.jsx
+++ front-end/src/pages/Register.jsx
@@ -1,9 +1,46 @@
 import React from 'react';
 
 export default function Register() {
+  const submit = (event) => {
+    event.preventDefault();
+  };
+
   return (
-    <div>
-      Regisro
-    </div>
+    <form>
+      <label
+        htmlFor="name"
+      >
+        Nome
+        <input
+          type="text"
+          data-testid="common_register__input-name"
+          id="name"
+        />
+      </label>
+      <label
+        htmlFor="email"
+      >
+        Email
+        <input
+          type="email"
+          data-testid=""
+          id="email"
+        />
+      </label>
+      <label
+        htmlFor="password"
+      >
+        Senha
+        <input
+          type="password"
+          id="password"
+        />
+      </label>
+      <input
+        type="submit"
+        value="CADASTRAR"
+        onClick={ submit }
+      />
+    </form>
   );
 }
