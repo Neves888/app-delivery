e588e8f53e3809d2f6d2e88acc6cedadb601067e
hsbicalho <hsbicalho@gmail.com>
2023-01-24 15:50:47 -0300

    create context

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,16 +3,19 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import DeliveryProvider from './context/DeliveryProvider';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route exact path="/" element={ <Login /> } />
-        <Route exact path="/login" element={ <Login /> } />
-        <Route exact path="/register" element={ <Register /> } />
-      </Routes>
-    </BrowserRouter>
+    <DeliveryProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={ <Login /> } />
+          <Route exact path="/login" element={ <Login /> } />
+          <Route exact path="/register" element={ <Register /> } />
+        </Routes>
+      </BrowserRouter>
+    </DeliveryProvider>
   );
 }
 
---
+++ front-end/src/context/DeliveryProvider.jsx
@@ -0,0 +1,24 @@
+import React, { useState, useMemo } from 'react';
+import PropTypes from 'prop-types';
+import DeliveryContext from './DeliveryContext';
+
+function DeliveryProvider({ children }) {
+  const [user, setUser] = useState({});
+
+  const contextValue = useMemo(() => ({
+    user,
+    setUser,
+  }), [user]);
+
+  return (
+    <DeliveryContext.Provider value={ contextValue }>
+      {children}
+    </DeliveryContext.Provider>
+  );
+}
+
+DeliveryProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default DeliveryProvider;
