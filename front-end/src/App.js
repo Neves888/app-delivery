df250fde56769e029f0385b78313583a48a95298
hsbicalho <hsbicalho@gmail.com>
2023-01-24 17:48:12 -0300

    open PR login Release

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,19 +3,16 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Login from './pages/Login';
 import Register from './pages/Register';
-import DeliveryProvider from './context/DeliveryProvider';
-
+// PR
 function App() {
   return (
-    <DeliveryProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route exact path="/login" element={ <Login /> } />
-          <Route exact path="/register" element={ <Register /> } />
-          <Route exact path="/" element={ <Login /> } />
-        </Routes>
-      </BrowserRouter>
-    </DeliveryProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/login" element={ <Login /> } />
+        <Route exact path="/register" element={ <Register /> } />
+        <Route exact path="/" element={ <Login /> } />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
