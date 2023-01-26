38df4744087280777e0cc901d31ca302d09b5a65
hsbicalho <hsbicalho@gmail.com>
2023-01-26 13:56:19 -0300

    change cors

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 
-const Middlewares = require('../middlewares/middlewares');
+// const Middlewares = require('../middlewares/middlewares');
 
 const loginRouter = require('./routes/user.router');
-const { corsOptions } = require('../utils/utils');
+// const { corsOptions } = require('../utils/utils');
 
 const app = express();
 
@@ -12,9 +12,9 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(express.json());
 
-app.use(Middlewares.corsOrigin);
+// app.use(Middlewares.corsOrigin);
 
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.get('/coffee', (_req, res) => res.status(418).end());
 app.use('/login', loginRouter);
