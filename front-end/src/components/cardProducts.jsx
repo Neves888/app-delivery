9900893ebb4132fef108313cbf0c60c760122369
Neves888 <helen.vneves@gmail.com>
2023-01-27 10:54:03 -0300

    add req 14

--- front-end/src/components/cardProducts.jsx
+++ front-end/src/components/cardProducts.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function ProductCard(params) {
   const {
@@ -7,12 +7,20 @@ export default function ProductCard(params) {
     price,
     urlImage,
   } = params;
+
+  const [quantity, setQuantity] = useState(0);
+
   return (
     <section>
       <p data-testid={ `customer_products__element-card-title-${id}` }>{ name }</p>
-      <p data-testid={ `customer_products__element-card-price-${id}` }>{ price }</p>
+      <p data-testid={ `customer_products__element-card-price-${id}` }>
+        { price.replace('.', ',') }
+      </p>
       <img
         data-testid={ `customer_products__img-card-bg-image-${id}` }
+        style={ {
+          width: '100px',
+        } }
         src={ urlImage }
         alt={ name }
       />
@@ -21,6 +29,7 @@ export default function ProductCard(params) {
           data-testid={ `customer_products__button-card-rm-item-${id}` }
           type="button"
           name="remove"
+          onClick={ () => setQuantity(quantity - 1) }
         >
           -
         </button>
@@ -29,11 +38,13 @@ export default function ProductCard(params) {
           type="number"
           name="quantityCard"
           id="quantityCard"
+          value={ quantity < 0 ? 0 : quantity }
         />
         <button
           data-testid={ `customer_products__button-card-add-item-${id}` }
           type="button"
           name="add"
+          onClick={ () => setQuantity(quantity + 1) }
         >
           +
         </button>
